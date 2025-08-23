4099f08873ff98480d94d61be8d25b33ec1136c4
techwithgene <techwithgene@gmail.com>
2025-08-23 18:36:06 -0400

    Adding chapter 4

--- Chapter_4_exercises.py
+++ Chapter_4_exercises.py
@@ -3,7 +3,10 @@ pizzas = ['pepperoni', 'plain', 'supreme']
 for pizza in pizzas:
     print(f'{pizza.title()} is one of my favorites!')
     print(f'We should order a {pizza.lower()} pizza together!\n')
+
+# 2 versions of output
 pizzas_list = ', '.join(pizzas)
-print(f'Basically, I love {pizzas[0]}, {pizzas[1]}, and {pizzas[2]} pizzas. Yay for pizza lovers!')
 print(f'Basically, I love {pizzas_list} pizzas. Yay for pizza lovers!')
+print(f'Basically, I love {pizzas[0]}, {pizzas[1]}, and {pizzas[2]} pizzas. Yay for pizza lovers!')
+
 
