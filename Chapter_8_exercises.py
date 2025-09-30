6ba85cd97fb2beb267709b508c6eb585ea6a9859
techwithgene <techwithgene@gmail.com>
2025-09-29 20:01:40 -0400

    Updates

--- Chapter_8_exercises.py
+++ Chapter_8_exercises.py
@@ -40,3 +40,71 @@ describe_city(city="Newark")
 describe_city(city="Los Angeles")
 describe_city(city="London", country="England")
 
+# city names
+def city_country(city, country):
+    full_location = f"{city.title()}, {country.title()} "
+    return full_location
+
+while True:
+    print("\nWhat is your location? Enter 'Quit' to exit program.")
+    user_city = input("First, tell me your city. ")
+    if user_city.lower() == "quit":
+        break
+
+    user_country = input("Next, tell me your country. ")
+    if user_country.lower() == "quit":
+        break
+
+    formatted_location = city_country(user_city, user_country)
+    print(f"Hello, your location is {formatted_location}.\n")
+
+# album v1
+# def make_album(artist, album, track_number=None):
+#     album_detail = {"artist_name": artist, "album_name": album}
+#     if track_number:
+#         album_detail["track_number"] = track_number
+#     return album_detail
+#
+# test1 = make_album("Me", "My", 5)
+# test2 = make_album("Mo", "Ma")
+# test3 = make_album("Mi", "Mu", 10)
+
+# print(test1)
+# print(test2)
+# print(test3)
+
+# album v2
+def make_album(artist, album, track_number=None):
+    album_detail = {"Artist Name": artist, "Album Name": album}
+    if track_number:
+        album_detail["track_number"] = track_number
+    return album_detail
+
+while True:
+    artist = input("What is the name of the musical artist? ")
+    if artist.lower() == "quit":
+        break
+    album = input("What is the title of the album? ")
+    if album.lower() == "quit":
+        break
+    track_number = input("Do you know how many tracks are on the album? "
+                         "If you don't know, press enter. ")
+    if track_number.lower() == "quit":
+        break
+
+    user_album = make_album(artist, album, track_number)
+    print("Here is the album info:", user_album)
+
+# Messages
+messages = ["Hello", "Goodbye", "Bye"]
+delivered_messages = []
+def send_messages(source_list, delivered_list):
+    while messages:
+        current_message = source_list.pop()
+        print(f"the following message is being delivered: {current_message}")
+        delivered_list.append(current_message)
+
+send_messages(messages, delivered_messages)
+
+print("The following messages have been delivered:\n")
+print(delivered_messages)
